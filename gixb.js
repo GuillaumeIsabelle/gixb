8292f2201052ea49b646ae5a34fa2443d164e25f
Guillaume Isabelle <jgi@jgwill.com>
2019-03-10 11:37:52 -0400

    fix path

--- gixb.js
+++ gixb.js
@@ -1,13 +1,24 @@
+#!/usr/bin/env node
+var shell = require("shelljs");
+var args = process.argv.slice(2);
+
+var f = args[0];
+
 //@v Browse a link
 //ADd this to your package.jsos
 // "mgm:firebase:hosting:site": "node browse.js \"https://tmyapp.firebaseapp.com/\""
 const opn = require('opn');
 
-var args = process.argv.slice(2);
 
-var f = args[0];
 
 opn(f ).then(() => {
   // image viewer closed
   console.log("Openning: " + f);
 });
+
+
+/*
+shell.exec(`pwd`);
+shell.exec(`node ../gixb/gixb.js "${f}"`);
+
+*/
