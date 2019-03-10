77e01f06bd474b3557e39112207e6b064273d501
Guillaume Isabelle <jgi@jgwill.com>
2019-03-10 12:03:02 -0400

    refacto : removed dependency & docs

--- gixb.js
+++ gixb.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
+
+/** Guillaume Isabelle GIX Browsing
+ * Vision: A Package you use to browse web in your app
+ * Value: Run action in your IDE to browse your resources in 1 step and few seconds
+ * Value: Keep track of your resources used in your package.json
+ * Current Reality: Various resources used such as dev site, various provider such as DevOps, DNS config, hosting, Staging to remember
+ */
+
+
 var shell = require("shelljs");
 var args = process.argv.slice(2);
 
 var f = args[0];
 
-//@v Browse a link
-//ADd this to your package.jsos
-// "mgm:firebase:hosting:site": "node browse.js \"https://tmyapp.firebaseapp.com/\""
+
 const opn = require('opn');
 
 
-
+//Opening
 opn(f ).then(() => {
-  // image viewer closed
+//output after opening
   console.log("GIX Browing Openning: " + f);
 });
 
 
-/*
-shell.exec(`pwd`);
-shell.exec(`node ../gixb/gixb.js "${f}"`);
 
-*/
